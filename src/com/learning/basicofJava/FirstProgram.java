dcc30b0465b6a6fe41245569a30869d1f7cbc93a
Oge <ogechukwuoba@gmail.com>
2024-09-02 19:11:58 -0400

    Commiting to New_OG_Branch for CRUD operation on Files

--- src/com/learning/basicofJava/FirstProgram.java
+++ src/com/learning/basicofJava/FirstProgram.java
@@ -4,8 +4,23 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Welcome to learning Java");
-        System.out.println("Hello World");
+       // System.out.println("Welcome to learning Java");
+       // System.out.println("Hello World");
+        
+        String Firstname = "Oge";
+        String Lastname = "Asiegbu";
+        String FullName = Firstname + " " + Lastname;
+        System.out.println("Full Name is: " + FullName);
+        
+        Firstname = "Oge";
+        Lastname = "Asiegbu";
+        FullName = Firstname + " " + Lastname;
+        System.out.println("Full Name is: " + FullName);
+        
+        Firstname = "Oge";
+        Lastname = "Asiegbu";
+        FullName = Firstname + " " + Lastname;
+        System.out.println("Full Name is: " + FullName);
 	}
 
 }
